e5c23590feddcde90c22cf5f500daef213f728d1
Marcela Tatiana Martínez <marce.t1994@gmail.com>
2023-11-16 12:09:55 -0600

    Pushing completed HTML

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 
 
-// Import necessary modules
+// Importing necessary modules
 
 const express = require('express');
 
 const path = require('path');
 
-// Create an Express application
+// Creating Express application
 
 const app = express();
 
 const PORT = 8000;
 
-// Define a route that sends the HTML file
+// Defining a route that sends the HTML file
 
 app.get('/', (req, res) => {
     const indexPath = path.join(__dirname, 'index.html');
@@ -20,7 +20,15 @@ app.get('/', (req, res) => {
     res.sendFile(indexPath);
 });
 
-// Start server and listen on the specified port
+// Serving static images and CSS
+
+app.use('/img', express.static(__dirname + '/Public/Images'));
+
+app.use('/styles', express.static(__dirname + '/Public/Styles'));
+
+app.use('/scripts', express.static(__dirname + '/Scripts'));
+
+// Starting server and listening on the specified port
 
 app.listen(PORT, () => {
     console.log(`Server is listening at http://localhost:${PORT}`);
